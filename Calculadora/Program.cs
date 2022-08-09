adeeb2dc2f3cd6056709bd70e07ac7ff771e1e6f
maahbatistaa <104997176+maahbatistaa@users.noreply.github.com>
2022-08-09 15:03:42 -0300

    adicionando biblioteca

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,50 +1,19 @@
 ﻿using System;
+using CalculatorLibrary;
 
-namespace Calculator
+namespace CalculatorProgram
 {
-    class Calculator
-    {
-        public static double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; //O valor padrão é 'não é um número' se uma operação, como divisão, pode resultar em um erro.
-
-            //Use uma instrução switch para fazer as contas
-            switch(op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break ;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    //Peça ao usuário para inserir um divisor diferente de zero
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    break ;
-                //Texto de retorno para uma entrada de opção incorreta.
-                default:
-                    break ;
-            }
-
-            return result;
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
             bool endApp = false;
-            // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            // Exibe o título como o aplicativo de calculadora do console C#.
+            Console.WriteLine("Calculadora do Console em C#\r");
+            Console.WriteLine("----------------------------\n");
 
+            Calculator calculator = new Calculator();
             while (!endApp)
             {
                 // Declare variables and set to empty.
@@ -53,55 +22,55 @@ namespace Calculator
                 double result = 0;
 
                 // Ask the user to type the first number.
-                Console.Write("Type a number, and then press Enter: ");
+                Console.Write("Digite um número e pressione Enter: ");
                 numInput1 = Console.ReadLine();
 
                 double cleanNum1 = 0;
                 while (!double.TryParse(numInput1, out cleanNum1))
                 {
-                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    Console.Write("Esta não é uma entrada válida. Por favor insira um valor inteiro: ");
                     numInput1 = Console.ReadLine();
                 }
 
                 // Ask the user to type the second number.
-                Console.Write("Type another number, and then press Enter: ");
+                Console.Write("Digite outro número e pressione Enter: ");
                 numInput2 = Console.ReadLine();
 
                 double cleanNum2 = 0;
                 while (!double.TryParse(numInput2, out cleanNum2))
                 {
-                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    Console.Write("Esta não é uma entrada válida. Por favor insira um valor inteiro: ");
                     numInput2 = Console.ReadLine();
                 }
 
                 // Ask the user to choose an operator.
-                Console.WriteLine("Choose an operator from the following list:");
-                Console.WriteLine("\ta - Add");
-                Console.WriteLine("\ts - Subtract");
-                Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
-                Console.Write("Your option? ");
+                Console.WriteLine("Escolha um operador na lista a seguir: ");
+                Console.WriteLine("\ta - Adição");
+                Console.WriteLine("\ts - Subtração");
+                Console.WriteLine("\tm - Multiplicação");
+                Console.WriteLine("\td - Divisão");
+                Console.Write("Qual opção? ");
 
                 string op = Console.ReadLine();
 
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                        Console.WriteLine("Esta operação resultará em um erro matemático.\n");
                     }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    else Console.WriteLine("Seu resultado: {0:0.##}\n", result);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    Console.WriteLine("Oh não! Ocorreu uma exceção ao tentar fazer as contas.\n - Detalhes: " + e.Message);
                 }
 
                 Console.WriteLine("------------------------\n");
 
                 // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                Console.Write("Pressione 'n' e Enter para fechar o aplicativo ou pressione qualquer outra tecla e Enter para continuar: ");
                 if (Console.ReadLine() == "n") endApp = true;
 
                 Console.WriteLine("\n"); // Friendly linespacing.
@@ -109,6 +78,4 @@ namespace Calculator
             return;
         }
     }
-
-
 }
